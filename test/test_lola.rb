d59951d93c2bbe1a72311e3e1a0d9825abbe08a6
Carlos Hernandez <carlos@recrea.es>
2011-12-09 19:25:57 +0000

    Initial commit to lola.

---
+++ test/test_lola.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+class TestLola < Test::Unit::TestCase
+  should "probably rename this file and start testing for real" do
+    flunk "hey buddy, you should probably rename this file and start testing for real"
+  end
+end
